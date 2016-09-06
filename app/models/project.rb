9856dbf98715f76d519ac7592c1e0619340d1fad
MVanderwood <m.b.vanderwood@gmail.com>
2016-09-06 17:53:47 -0500

    Develop Project model and basic specifications.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,21 @@
 class Project < ApplicationRecord
+  def completed_tasks
+    tasks.where(completed: true)
+  end
+
+  def incompleted_tasks
+    tasks.where(completed: false)
+  end
+
+  def tasks(options={})
+    tasks = TdTask.where(project_id: id)
+    if options[:priority] == "ascending"
+      #Lower numbers are considered higher priority
+      tasks.order(:priority)
+    elsif options[:priority] == "descending"
+      tasks.order(priority: :desc)
+    else
+      tasks
+    end
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Project, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:project)).to be_valid
+  end
+
+  describe '#attributes' do
+    it 'has a description' do
+      expect(build(:project)).to respond_to(:description)
+    end
+  end
+
+  describe '#tasks' do
+    before :each do
+      @project = create(:project)
+    end
+
+    it 'provides a collection of tasks associated with the project' do
+      @project.tasks.each { |t| expect(t).to be_kind_of(TdTask) }
+    end
+
+    it 'sorts in ascending order if needed' do
+      tasks = @project.tasks(priority: "ascending")
+      expect(tasks[0].priority <= tasks[1].priority).to eq(true)
+    end
+
+    it 'sorts in descending order if needed' do
+      tasks = @project.tasks(priority: "descending")
+      expect(tasks[0].priority >= tasks[1].priority).to eq(true)
+    end
+  end
+
+  describe '#completed_tasks' do
+    it 'provides a collection of completed_tasks' do
+      project = create(:project)
+      project.tasks[0].update(completed: true)
+      project.tasks[1].update(completed: true)
+      expect(project.completed_tasks.length).to eq(2)
+      project.completed_tasks.each do |t|
+        expect(t.completed).to eq(true)
+      end
+    end
+  end
 end
